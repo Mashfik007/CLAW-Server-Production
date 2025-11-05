b8d8e6996371bb628480fdffcf0fa1e201c377e5
Mashfik007 <webwizardryh007q@gmail.com>
2025-11-05 21:30:49 +0600

    added

--- Router/IMage Sample.js
+++ Router/IMage Sample.js
@@ -3,7 +3,6 @@ const upload = require("../MIddlewares/File-Upload-Multer")
 
 router.post('/image', upload.array('image', 2), async (req, res) => {
  try {
-
   res.send("image uploaded")
  } catch (error) {
   res.status(500).send(error.message)
